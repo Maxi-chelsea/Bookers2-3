f37daee9d08dbdbe16f1424a0a2fc907ccd272ca
Maxi-chelsea <maxi19950049@gmail.com>
2021-06-28 13:45:14 +0000

    update_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
